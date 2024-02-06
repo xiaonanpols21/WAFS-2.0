099fd65387a1ac7236065c35fa81504360770016
xiaonanpols21 <xiaonanpolspols@gmail.com>
2024-02-06 15:05:17 +0100

    opstarten ejs

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+app.use(express.static('public'))
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
